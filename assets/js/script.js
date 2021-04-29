afbf78b3a0cf4134b2b170f7f605cbab9a6d4ec3
tielkingc <tielking5@gmail.com>
2021-04-29 11:38:43 -0400

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function(){
+    var listItemEl = document.createElement("li");
+    listItemEl.textContent = "This is a new task.";
+    listItemEl.className = "task-item";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener('click', createTaskHandler);
